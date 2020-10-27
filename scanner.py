651b5166cb5e95ed933ee67a2b6cb37c8dd1bb24
Apocalypsor <37431235+Apocalypsor@users.noreply.github.com>
2020-10-27 15:11:03 +0800

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -6,14 +6,17 @@ import os
 
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 
+
 def blockPrint():
-    sys.stdout = open(os.devnull, 'w')
+    sys.stdout = open(os.devnull, "w")
+
 
 def enablePrint():
     sys.stdout = sys.__stdout__
 
+
 def availableQuery(x, domain_suff):
-    pref = ''.join(x)
+    pref = "".join(x)
     domain = pref + domain_suff
     retries = 0
 
@@ -23,33 +26,33 @@ def availableQuery(x, domain_suff):
             response = whois.whois(domain)
             return response
             enablePrint()
-            if response == 'Socket not responding':
+            if response == "Socket not responding":
                 retries += 1
             else:
                 retries = 3
         except whois.parser.PywhoisError as e:
-            print('\n  Available:', domain, '|', str(e).split('\n')[0])
-            return domain + '\n'
+            print("\n  Available:", domain, "|", str(e).split("\n")[0])
+            return domain + "\n"
         except:
             retries += 1
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     domain_length = 2
-    domain_suff = '.moe'
+    domain_suff = ".moe"
 
-    print('--------------------Start!--------------------')
+    print("--------------------Start!--------------------")
     domain_pref = tuple(product(alphabet, repeat=domain_length))
     domain_suff = [domain_suff for _ in domain_pref]
 
     result = p_umap(availableQuery, domain_pref, domain_suff)
 
-    print('--------------------Done!--------------------')
+    print("--------------------Done!--------------------")
 
     result = set(result).remove(None)
 
     if result:
-        with open('domains.txt', 'w') as f:
+        with open("domains.txt", "w") as f:
             f.writelines(result)
     else:
-        print('Nothing found!')
+        print("Nothing found!")
