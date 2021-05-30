54ed8f837795b2d7cf8eff208d8f65ee29065a0d
Apocalypsor <37431235+Apocalypsor@users.noreply.github.com>
2021-05-30 20:07:48 +0800

    Update check.py

--- Check/check.py
+++ Check/check.py
@@ -1,14 +1,24 @@
 from .utils import getUrl
+import requests
 
 def check(domain):
     url = "https://who.is/whois/" + domain
-    x = getUrl(url)
+    
+    try:
+        x = getUrl(url)
 
-    if "No match for" in x.text or "NOT FOUND" in x.text:
-        print(f"Avaliable: {domain}\n")
-        with open("domain.txt", "a") as write:
+        if "No match for" in x.text or "NOT FOUND" in x.text:
+            print(f"Avaliable: {domain}\n")
+            with open("domain.txt", "a") as write:
+                    write.write(domain + "\n")
+            return True
+        else:
+            print(f"Unavaliable: {domain}\n")
+            return False
+    except requests.exceptions.ConnectionError:
+        print(f"Error: {domain}\n")
+        with open("error.txt", "a") as write:
                 write.write(domain + "\n")
-        return True
-    else:
-        print(f"Unavaliable: {domain}\n")
-        return False
+    except:
+        pass
+        
